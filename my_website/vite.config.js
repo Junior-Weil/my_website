f4e0689a93ff6e78a19ac9dd99f028feac17e5a1
Junior-Weil <rmw020@bucknell.edu>
2025-08-11 13:42:26 -0500

    fixing font error, pass inline font as file

--- my_website/vite.config.js
+++ my_website/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/my_website/',
+  build: { assetsInlineLimit: 0 },
 
   define: {
     'import.meta.env.TS_BASE_URL': JSON.stringify('/my_website'),
